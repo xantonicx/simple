352c4c37687e4ea03e52b824b85ed70c9e92b5f0
Anthony Iverson <xantonicx@gmail.com>
2024-10-25 17:58:44 -0500

    backtracked

--- theblog/models.py
+++ theblog/models.py
@@ -4,8 +4,6 @@ from django.contrib.auth.models import User
 class Post(models.Model):
 	title = models.CharField(max_length=255)
 	title_tag = models.CharField(max_length=255, default="default")
-	#title_tag = models.CharField(max_length=255, default="2024-10-10")
-	author = models.ForeignKey(User, on_delete=models.CASCADE)
 	body = models.TextField()
 
 	def __str__(self):
